8ab306c2073858524f87e0ba43e01278bdc9ca41
David DeSandro <desandrocodes@gmail.com>
2015-02-04 16:12:54 -0500

    bands

---
+++ bands/diamond-dots-switch.js
@@ -0,0 +1,162 @@
+var TAU = Math.PI * 2;
+var ROOT_2 = Math.sqrt( 2 );
+
+var img = new Image();
+var canvas = document.createElement('canvas');
+var ctx = canvas.getContext('2d');
+var imgData;
+var size = 1 / 16; /* proportion of img width */
+var repeatFrames = 16;
+var frame = 0;
+var zoom;
+img.onload = imgLoaded;
+
+var w, h;
+
+window.onload = function() {
+  img.src = '../img/bands3.jpg';
+};
+
+function imgLoaded() {
+  w = canvas.width = img.width;
+  h = canvas.height = img.height;
+
+  zoom = 540 / w;
+
+  ctx.drawImage( img, 0, 0 );
+  imgData = ctx.getImageData( 0, 0, w, h ).data;
+
+  // apply zoom
+  w *= zoom;
+  h *= zoom;
+  canvas.width = w;
+  canvas.height = h;
+
+  render();
+
+  document.body.appendChild( canvas );
+}
+
+
+function render() {
+  
+  frame++;
+
+  ctx.fillStyle = 'black';
+  ctx.fillRect( 0, 0, w, h );
+  var pixelSize = w * size;
+  var cols = Math.ceil( w / pixelSize );
+  var rows = Math.ceil( h / (pixelSize / 2) );
+  var dotColor, squareColor, dotSize;
+  var diamondOffset = pixelSize * 0.53;
+
+  var frameOffsetY = (pixelSize) * ( ( frame % repeatFrames ) / repeatFrames );
+
+  for ( var row = 0; row < rows; row++ ) {
+    for ( var col = 0; col < cols; col++ ) {
+      var x = ( col + 0.5 ) * pixelSize;
+      x += row % 2 ? pixelSize / 2 : 0;
+
+      var y = ( row + 0.5 ) * (pixelSize / 2);
+      y += frameOffsetY;
+      // x -= frameOffsetY;
+      var pixelData = getPixelData( x / zoom, y / zoom );
+      var hsl = rgb2hsl( pixelData.red, pixelData.green, pixelData.blue );
+      // var hueColor = 'hsl(' + hsl.hue + ',100%,50%)';
+      // var sat = ( hsl.sat * 0.5 + 0.5 ) * 100;
+      var sat = 100;
+      // var hue = Math.round( Math.round( hsl.hue / 120 ) * 120);
+      var hueColor = 'hsl(' + hsl.hue + ',' + sat + '%,50%)';
+      
+      if ( hsl.lum < 0.25 ) {
+        // color dot on black
+        squareColor = 'black';
+        dotColor = hueColor;
+        dotSize = hsl.lum / 0.25;
+      } else if ( hsl.lum < 0.5 ) {
+        // black dot on color
+        squareColor = hueColor;
+        dotColor = 'black';
+        dotSize = ( 0.5 - hsl.lum )  / 0.25;
+      } else if ( hsl.lum < 0.75 ) {
+        // white dot on color
+        squareColor = hueColor;
+        dotColor = 'white';
+        dotSize = ( hsl.lum - 0.5 )  / 0.25;
+      } else {
+        // color dot on white
+        squareColor = 'white';
+        dotColor = hueColor;
+        dotSize = ( 1 - hsl.lum ) / 0.25;
+      }
+
+      dotSize = Math.max( dotSize, 0 );
+
+      // ctx.fillStyle = 'rgb(' + pixelData.red + ',' + pixelData.green + ',' +
+      //   pixelData.blue + ')';
+      // ctx.fillStyle = 'hsl(' + hsl.hue + ',' + (hsl.sat*100) + '%,' +
+      //   (hsl.lum*100) + '%)';
+      // square
+      ctx.fillStyle = squareColor;
+      // var rectX = x - pixelSize * 0.5;
+      // var rectY = y - pixelSize * 0.5;
+      ctx.beginPath();
+      ctx.moveTo( x, y - diamondOffset );
+      ctx.lineTo( x + diamondOffset, y );
+      ctx.lineTo( x, y + diamondOffset );
+      ctx.lineTo( x - diamondOffset, y );
+      ctx.lineTo( x, y - diamondOffset );
+      ctx.fill();
+      ctx.closePath();
+      // ctx.fillRect( rectX, rectY, pixelSize, pixelSize );
+      // dot
+      var dotRadius = Math.sqrt( (pixelSize * pixelSize) / TAU ) * dotSize * 1 / ROOT_2;
+      ctx.fillStyle = dotColor;
+      ctx.beginPath();
+      ctx.arc( x, y, dotRadius, 0, TAU );
+      ctx.fill();
+      ctx.closePath();
+    }
+  }
+
+  // setTimeout( render, 17 )
+}
+
+function getPixelData( x, y ) {
+  x = Math.round( x );
+  y = Math.round( y );
+  x = Math.max( 0, Math.min( img.width, x ) );
+  y = Math.max( 0, Math.min( img.height, y ) );
+  var pixelIndex = x + y * img.width;
+  pixelIndex *= 4;
+  return {
+    red: imgData[ pixelIndex + 0 ],
+    green: imgData[ pixelIndex + 1 ],
+    blue: imgData[ pixelIndex + 2 ],
+    alpha: imgData[ pixelIndex + 3 ]
+  };
+}
+
+// https://github.com/jfsiii/chromath/blob/master/src/chromath.js
+function rgb2hsl( r, g, b ) {
+  r /= 255;
+  g /= 255;
+  b /= 255;
+  var M = Math.max(r, g, b);
+  var m = Math.min(r, g, b);
+  var C = M - m;
+  var L = 0.5*(M + m);
+  var S = (C === 0) ? 0 : C/(1-Math.abs(2*L-1));
+
+  var h;
+  if (C === 0) h = 0;
+  else if (M === r) h = ((g-b)/C) % 6;
+  else if (M === g) h = ((b-r)/C) + 2;
+  else if (M === b) h = ((r-g)/C) + 4;
+
+  var H = 60 * h;
+
+  return { hue: H, sat: parseFloat(S), lum: parseFloat(L) };
+  
+  // } [H, parseFloat(S), parseFloat(L)];
+}
---
+++ bands/halftone1.js
@@ -0,0 +1,199 @@
+/*jshint browser: true, devel: true, unused: true, undef: true */
+
+( function( window ) {
+
+var TAU = Math.PI * 2;
+var ROOT_2 = Math.sqrt( 2 );
+
+var img = new Image();
+var canvas = document.createElement('canvas');
+var ctx = canvas.getContext('2d');
+var imgData;
+var zoom;
+img.onload = imgLoaded;
+
+var isAdditive = true;
+
+var repeatFrames = 12;
+
+window.onload = function() {
+  img.src = '../img/bands5.jpg';
+};
+
+var w, h, diag, renderCanvases;
+
+function imgLoaded() {
+  w = canvas.width = img.width;
+  h = canvas.height = img.height;
+
+
+  ctx.drawImage( img, 0, 0 );
+  imgData = ctx.getImageData( 0, 0, w, h ).data;
+
+  zoom = 540 / w;
+
+  // zoom
+  w *= zoom;
+  h *= zoom;
+  canvas.width = w;
+  canvas.height = h;
+  var side = Math.max( w, h );
+  diag = side * ROOT_2;
+  ctx.clearRect( 0, 0, w, h );
+
+  //
+  renderCanvases = {
+    red: getRenderCanvas(),
+    green: getRenderCanvas(),
+    blue: getRenderCanvas()
+  };
+
+  // render each layout
+  document.body.appendChild( canvas );
+  render();
+
+}
+
+var frame = 0  ;
+
+function render() {
+  frame++;
+
+  var cm = w / 100;
+
+  renderGrid( 1, 'red', 5 * cm, 'circle' );
+  renderGrid( 6, 'green', 8 * cm, 'circle' );
+  renderGrid( 4, 'blue', 7 * cm, 'circle' );
+
+  ctx.globalCompositeOperation = 'source-over';
+  ctx.fillStyle = isAdditive ? 'black' : 'white';
+  ctx.fillRect( 0, 0, w, h );
+  ctx.globalCompositeOperation = isAdditive ? 'lighter' : 'darker';
+  ctx.drawImage( renderCanvases.red.canvas, 0, 0 );
+  ctx.drawImage( renderCanvases.green.canvas, 0, 0 );
+  ctx.drawImage( renderCanvases.blue.canvas, 0, 0 );
+  setTimeout( render, 17 );
+}
+
+window.render = render;
+window.ctx = ctx;
+
+function getRenderCanvas() {
+  var renderCanvas = document.createElement('canvas');
+  renderCanvas.width = w;
+  renderCanvas.height = h;
+  return {
+    canvas: renderCanvas,
+    ctx: renderCanvas.getContext('2d')
+  };
+}
+
+function renderGrid( angle, color, spacing, shape ) {
+  var renderCtx = renderCanvases[ color ].ctx;
+  renderCtx.fillStyle = isAdditive ? 'black' : 'white';
+  renderCtx.fillRect( 0, 0, w, h );
+  var cols = Math.ceil( diag / spacing );
+  var rows = Math.ceil( diag / spacing );
+
+  switch ( color ) {
+    case 'red' :
+      renderCtx.fillStyle = isAdditive ? 'rgb(255,0,0)' : 'rgb(0,255,255)';
+      break;
+    case 'green' :
+      renderCtx.fillStyle = isAdditive ? 'rgb(0,255,0)' : 'rgb(255,0,255)';
+      break;
+    case 'blue' :
+      renderCtx.fillStyle = isAdditive ? 'rgb(0,0,255)' : 'rgb(255,550,0)';
+      break;
+  }
+
+  var renderShape = shapeRenders[ shape ];
+
+  // var mod = ( frame % repeatFrames ) / repeatFrames || 1;
+  for ( var row = 0; row < rows; row++ ) {
+    for ( var col = 0; col < cols; col++ ) {
+      if ( color == 'red' && row % 2 ) {
+        continue
+      }
+      var x1 = col * spacing + spacing / 2;
+      var y1 = row * spacing + spacing / 2;
+      // move by x
+      x1 += ((frame % repeatFrames) / repeatFrames) * spacing;
+      // offset for diagonal
+      x1 -= ( diag - w ) / 2;
+      y1 -= ( diag - h ) / 2;
+      // shift to center
+      x1 -= w / 2;
+      y1 -= h / 2;
+      // rotate grid
+      var x2 = x1 * Math.cos( angle ) - y1 * Math.sin( angle );
+      var y2 = x1 * Math.sin( angle ) + y1 * Math.cos( angle );
+      // shift back
+      x2 += w / 2;
+      y2 += h / 2;
+      if ( x2 > -spacing && x2 < w + spacing && y2 > -spacing && y2 < h + spacing ) {
+        var x3 = x2 / zoom;
+        var y3 = y2 / zoom;
+        var pixelData = getPixelData( x3, y3 );
+        var colorSize = pixelData[ color ] / 255;
+        colorSize = isAdditive ? colorSize : 1 -colorSize;
+        // colorSize *= color === 'green' ? 0.5 : 1;
+        // colorSize = color === 'green' ? 0.5 : 1;
+        // render dot
+        var size = colorSize * spacing;
+        size *= color == 'green' ? 0.66 : 1;
+        renderShape( renderCtx, x2, y2, size, angle, spacing );
+
+      }
+    }
+  }
+}
+
+// hash of functions that render the shape
+var shapeRenders = {
+
+  circle: function( ctx, x, y, size ) {
+    size *= ROOT_2 / 2;
+    ctx.beginPath();
+    ctx.arc( x, y, size, 0, TAU );
+    ctx.fill();
+    ctx.closePath();
+  },
+  
+  line: function( ctx, x, y, size, angle, spacing ) {
+    ctx.save();
+    ctx.translate( x, y );
+    ctx.rotate( angle );
+    ctx.fillRect( -spacing, -size / 2, spacing+1, size  );
+    ctx.restore();
+  },
+
+  square: function( ctx, x, y, size, angle ) {
+    ctx.save();
+    ctx.translate( x, y );
+    ctx.rotate( angle );
+    ctx.fillRect( -size / 2, -size / 2, size, size );
+    ctx.restore();
+  }
+
+};
+
+function getPixelData( x, y ) {
+  x = Math.round( x );
+  y = Math.round( y );
+  x = Math.max( 0, Math.min( img.width, x ) );
+  y = Math.max( 0, Math.min( img.height, y ) );
+  var pixelIndex = x + y * img.width;
+  pixelIndex *= 4;
+  return {
+    red: imgData[ pixelIndex + 0 ],
+    green: imgData[ pixelIndex + 1 ],
+    blue: imgData[ pixelIndex + 2 ],
+    alpha: imgData[ pixelIndex + 3 ]
+  };
+}
+
+window.imgData = imgData;
+window.getPixelData = getPixelData;
+
+})( window );
